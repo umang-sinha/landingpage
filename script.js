dfce7079f4b72e2a54cfff2b4305eadd1f4660e8
umang-sinha <umangsinha12@gmail.com>
2020-12-22 01:46:13 +0530

    renamed the toggle() function to toggleImage() and added an animation that appears when sidebar is clicked

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var image = document.querySelector('.image');
+var bar = document.querySelector('.bar');
+var sideBar = document.querySelector('.side-bar');
 
-function toggle(e){
+//renamed toggle function to toggleImage
+function toggleImage(e){
     var source = e.getAttribute('data-image');
     image.src = source;
 }
+
+bar.addEventListener('click', function(){
+    sideBar.classList.toggle('side-bar-js');
+});
